2d1d65f662a8ad0a71f4e659921a0a17d0173494
PabloDDQ <pabloduartequadros@gmail.com>
2026-05-18 08:01:28 -0300

    Modificação do main.java de estrutura repetitiva

--- Curso Java - Trainee/Estrutura-Repetitiva/src/Main.java
+++ Curso Java - Trainee/Estrutura-Repetitiva/src/Main.java
@@ -1,5 +1,53 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /* 
+        int numeroAdivinha = 0, tentativas = 4;
+
+        while (tentativas >= 0) {
+            System.out.println("Você tem  " + (tentativas-- + 1) + " tentativas");
+            System.out.print("Digite o número: ");
+            numeroAdivinha = scanner.nextInt();
+
+            if (numeroAdivinha == 7) {
+                System.out.println("Você venceu!");
+                break;
+            }
+        }
+
+        if (numeroAdivinha != 7) {
+            System.out.println("Você perdeu! \nSuas tentativas acabaram.");
+        }
+
+        System.out.println("Fim de Jogo!");
+
+        */
+
+        //Exercício 01
+        /* 
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i++);
+            
+        }
+
+        */
+       //Exercício 2
+       /* 
+       for (int i = 0; i > 5; i++) {
+        for (int j = 0; i > 5; j++) {
+            System.out.print("*");
+            
+        }
+        */
+           
+       }
+
+
     }
+
 }
